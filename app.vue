11be984c17d0aca5fb625e5cf30fbc3f23231d4d
ziaul-hassan <ziaulhasanf@gmail.com>
2025-01-06 17:40:22 +0600

    tailwind intregrate with nuxt

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="bg-white h-2/3">
+        <div >
+            <img class="h-24 w-44 flex justify-center items-center mx-auto" src="E:\Html CSS\BD-Funnel-Builder-task_tailwind_css\assets\css\logo.png"><br>
+        </div>
+        <div class="text-[#1b4b36] text-center text-[60px] font-[700]">
+            <p>স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!
+                "আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</p><br>
+        </div>
+        <div class="">
+            
+            <p class="h-[50px] text-3xl mx-auto flex justify-center bg-yellow-400 font-[600] w-[556px] text-center">আপনার জীবনকে আরও স্মার্ট করুন।</p><br>
+            <iframe class= "mx-auto w-[1000px] h-[606px] top-[605px] left-[460px] gap-0 rounded-tl-[20px] rounded-tr-none rounded-bl-none rounded-br-none border-[2px] border-t border-opacity-0 flex justify-center flex-col items-center m-0" src="https://www.youtube.com/embed/lecHBYPpjZM?si=WIWqMhUwmJHamjZo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br>
+        </div>
+         <div class="text3-div">
+            <p class="font-bold flex flex-col justify-center items-center border-[1px] bg-[#1b4b36] rounded-[45px] w-[400px] mx-auto text-yellow-400 text-[28px]">অর্ডার করতে ক্লিক করুন<i class="fa fa-hand-o-right"></i></p>
+         </div>
+        
+     </section>
+</template>
