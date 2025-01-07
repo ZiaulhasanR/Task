c4ed7af424ad6011e3e3a1bcadbf290318d7cf1a
ziaul-hassan <ziaulhasanf@gmail.com>
2025-01-07 14:23:00 +0600

    tailwind intregrate with nuxt

--- app.vue
+++ app.vue
@@ -1,20 +1,145 @@
 <template>
-  <section class="bg-white h-2/3">
-        <div >
-            <img class="h-24 w-44 flex justify-center items-center mx-auto" src="E:\Html CSS\BD-Funnel-Builder-task_tailwind_css\assets\css\logo.png"><br>
+  <section class="bg-white xl:h-[1200px] md:h-fit sm:h-fit lg:h-fit">
+        <div > 
+            <img class="xl:h-24 xl:w-44 md:h-[96px] md:w-[176px] sm:h-[86px] sm:w-[136px] flex justify-center items-center mx-automx-auto mx-auto" src="E:\Html CSS\BD-Funnel-Builder-task_tailwind_css\assets\css\logo.png"><br>
         </div>
-        <div class="text-[#1b4b36] text-center text-[60px] font-[700]">
+        <div class="text-[#1b4b36] text-center  xl:font-[700] md:text-[60px] text-[36px] md:font-[500]">
             <p>স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!
                 "আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</p><br>
         </div>
         <div class="">
             
-            <p class="h-[50px] text-3xl mx-auto flex justify-center bg-yellow-400 font-[600] w-[556px] text-center">আপনার জীবনকে আরও স্মার্ট করুন।</p><br>
-            <iframe class= "mx-auto w-[1000px] h-[606px] top-[605px] left-[460px] gap-0 rounded-tl-[20px] rounded-tr-none rounded-bl-none rounded-br-none border-[2px] border-t border-opacity-0 flex justify-center flex-col items-center m-0" src="https://www.youtube.com/embed/lecHBYPpjZM?si=WIWqMhUwmJHamjZo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br>
+            <p class="h-[50px] text-3xl mx-auto flex justify-center bg-[#ebff3d] font-[600] w-[556px] text-center">আপনার জীবনকে আরও স্মার্ট করুন।</p><br>
+            <div class="max-w-[85%] w-[55%] aspect-[16/9] items-center flex flex-col justify-center relative mx-auto">
+                <iframe class= "absolute w-full h-full items-center" src="https://www.youtube.com/embed/lecHBYPpjZM?si=WIWqMhUwmJHamjZo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+            <br>
         </div>
-         <div class="text3-div">
-            <p class="font-bold flex flex-col justify-center items-center border-[1px] bg-[#1b4b36] rounded-[45px] w-[400px] mx-auto text-yellow-400 text-[28px]">অর্ডার করতে ক্লিক করুন<i class="fa fa-hand-o-right"></i></p>
-         </div>
+        <div class="flex flex-col">
+            <p class="font-bold flex justify-center items-center border-[1px] bg-[#1b4b36] rounded-[45px] w-[400px] mx-auto text-[#ebff3d] text-[28px]">
+                অর্ডার করতে ক্লিক করুন 
+            <img class="h-[35px] w-[45px] ml-2" src="E:\Html CSS\BD-Funnel-Builder-task_tailwind_css\assets\css\hand.png">
+            </p>
+        </div>
+
         
      </section>
+
+     <section class= "xl:h-[1000px] bg-[#faffce] md:h-fit sm:h-fit lg:h-fit">
+
+        <div class=" ">
+            <p class="font-[700] text-[40px] text-[#1b4b36] mx-auto pl-[180px]">স্মার্টওয়াচের একাধিক ফিচার এক নজরে</p>
+        </div>
+        <div class=" flex md:flex-row flex-col bg-[#faffce] justify-around flex-wrap px-[150px] py-[79px] gap-x-10 gap-y-10">
+        <div class="group lg:max-w-[306px] lg:h-[297px] border-[3px] md:max-w-[80%] border-[#1b4b36] bg-[#faffce] rounded-tl-[35px] rounded-br-[35px] p-4 shadow-md hover:bg-[#1b4b36] hover:[text-shadow:0_0_25px_#1b4b36,0_0_50px_#1b4b36]">
+            <p class="font-bold text-[25px] mb-2 text-[#1b4b36] group-hover:text-[#ebff3d]">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</p>
+            <p class="text-[16px] text-[#090101] group-hover:text-white">স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড। ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ নির্ধারণ করুন। স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।</p>
+        </div>
+
+        <div class="group lg:max-w-[306px] lg:h-[297px] border-[3px] md:max-w-[80%] border-[#1b4b36] bg-[#faffce] rounded-tl-[35px] rounded-br-[35px] p-4 shadow-md hover:bg-[#1b4b36] hover:[text-shadow:0_0_25px_#1b4b36,0_0_50px_#1b4b36]">
+            <p class="font-bold text-[25px] mb-2 text-[#1b4b36] group-hover:text-[#ebff3d]">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</p>
+            <p class="text-[16px] text-[#090101] group-hover:text-white">স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড। ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ নির্ধারণ করুন। স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।</p>
+        </div>
+
+        <div class="group lg:max-w-[306px] lg:h-[297px] border-[3px] md:max-w-[80%] border-[#1b4b36] bg-[#faffce] rounded-tl-[35px] rounded-br-[35px] p-4 shadow-md hover:bg-[#1b4b36] hover:[text-shadow:0_0_25px_#1b4b36,0_0_50px_#1b4b36]">
+            <p class="font-bold text-[25px] mb-2 text-[#1b4b36] group-hover:text-[#ebff3d]">ফিটনেস ট্র্যাকিং</p>
+            <p class="text-[16px] text-[#090101] group-hover:text-white">স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড। ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ নির্ধারণ করুন। স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।</p>
+        </div>
+
+        <div class="group lg:max-w-[306px] lg:h-[297px] border-[3px] md:max-w-[80%] border-[#1b4b36] bg-[#faffce] rounded-tl-[35px] rounded-br-[35px] p-4 shadow-md hover:bg-[#1b4b36] hover:[text-shadow:0_0_25px_#1b4b36,0_0_50px_#1b4b36]">
+            <p class="font-bold text-[25px] mb-2 text-[#1b4b36] group-hover:text-[#ebff3d]">স্মার্ট নোটিফিকেশন</p>
+            <p class="text-[16px] text-[#090101] group-hover:text-white">কল, মেসেজ, এবং অ্যাপ নোটিফিকেশন সরাসরি আপনার হাতে। গুরুত্বপূর্ণ আপডেট কখনোই মিস করবেন না।</p>
+        </div>
+
+        <div class="group lg:max-w-[306px] lg:h-[297px] border-[3px] md:max-w-[80%] border-[#1b4b36] bg-[#faffce] rounded-tl-[35px] rounded-br-[35px] p-4 shadow-md hover:bg-[#1b4b36] hover:[text-shadow:0_0_25px_#1b4b36,0_0_50px_#1b4b36]">
+            <p class="font-bold text-[25px] mb-2 text-[#1b4b36] group-hover:text-[#ebff3d]">দীর্ঘস্থায়ী ব্যাটারি লাইফ</p>
+            <p class="text-[16px] text-[#090101] group-hover:text-white">একবার চার্জে ৭+ দিন ব্যবহারের সুবিধা। দ্রুত চার্জিং প্রযুক্তি।</p>
+        </div>
+
+        <div class="group lg:max-w-[306px] lg:h-[297px] border-[3px] md:max-w-[80%] border-[#1b4b36] bg-[#faffce] rounded-tl-[35px] rounded-br-[35px] p-4 shadow-md hover:bg-[#1b4b36] hover:[text-shadow:0_0_25px_#1b4b36,0_0_50px_#1b4b36]">
+            <p class="font-bold text-[25px] mb-2 text-[#1b4b36] group-hover:text-[#ebff3d]">মিউজিক এবং ক্যামেরা নিয়ন্ত্রণ</p>
+            <p class="text-[16px] text-[#090101] group-hover:text-white">মিউজিক চালানো বা ক্যামেরা ক্লিক করুন আপনার স্মার্টওয়াচ থেকে। সহজ এবং সাবলীল ব্যবহারের সুবিধা।</p>
+        </div>
+        </div>
+
+
+        <div class="flex flex-col">
+            <p class="font-bold flex justify-center items-center border-[1px] bg-[#1b4b36] rounded-[45px] w-[400px] mx-auto text-[#ebff3d] text-[28px]">
+                অর্ডার করতে ক্লিক করুন 
+            <img class="h-[35px] w-[45px] ml-2" src="E:\Html CSS\BD-Funnel-Builder-task_tailwind_css\assets\css\hand.png">
+            </p>
+        </div>
+    </section>
+
+    <section class= "xl-h-[900px] bg-[#ffffff] md:h-fit sm:h-fit lg:h-fit">
+        <div>
+            <p class="font-[700] text-[38px] text-[black] mx-auto text-center gap-[50px]">আমাদের পণ্য নিয়ে গ্রাহকদের অভিজ্ঞতা</p>
+        </div>
+        <div class="flex flex-wrap justify-around bg-white px-[180px] pt-[79px] gap-5 ">
+        <div class="bg-[#1b4b36] pl-[10px] pt-[20px] w-[467px] h-[280px] m-2 rounded-[20px] flex flex-col opacity-100 transition-all duration-300 ease-linear">
+            <p class="text-left text-[#f3fffa] pl-2 font-bold text-[25px] mb-2">সন্তুষ্ট গ্রাহক</p><br><br>
+            <p class="text-right text-[#ebff3d] font-bold text-[45px] mb-4 pr-[10px]">৩,০০০+</p>
+            <hr class="w-full border-[#f3fffa] mb-4">
+            <p class="text-center text-[#f3fffa] text-[17px]">সন্তুষ্ট গ্রাহকরা আমাদের পণ্যের প্রতি বিশ্বাস রেখেছেন।</p>
+        </div>
+
+        <div class="bg-[#1b4b36] pl-[10px] pt-[20px] w-[467px] h-[280px] m-2 rounded-[20px] flex flex-col opacity-100 transition-all duration-300 ease-linear">
+            <p class="text-left text-[#f3fffa] pl-2 font-bold text-[25px] mb-2">পুনঃক্রয়কারী</p><br><br>
+            <p class="text-right text-[#ebff3d] font-bold text-[45px] mb-4 pr-[10px]">৪০০+</p>
+            <hr class="w-full border-[#f3fffa] mb-4">
+            <p class="text-center text-[#f3fffa] text-[17px]">পুনঃক্রয় করে আমাদের গুণমান নিশ্চিত করে।</p>
+        </div>
+
+        <div class="bg-[#1b4b36] pl-[10px] pt-[20px] w-[467px] h-[280px] m-2 rounded-[20px] flex flex-col opacity-100 transition-all duration-300 ease-linear">
+            <p class="text-left text-[#f3fffa] pl-2 font-bold text-[25px] mb-2">পণ্য বিক্রি</p><br><br>
+            <p class="text-right text-[#ebff3d] font-bold text-[45px] mb-4 pr-[10px]">৫০০০+</p>
+            <hr class="w-full border-[#f3fffa] mb-4">
+            <p class="text-center text-[#f3fffa] text-[17px]">৫০০০ পণ্যের বিক্রি আমাদের জনপ্রিয়তা প্রমাণ করে।</p>
+        </div>
+
+        <div class="bg-[#1b4b36] pl-[10px] pt-[20px] w-[467px] h-[280px] m-2 rounded-[20px] flex flex-col opacity-100 transition-all duration-300 ease-linear">
+            <p class="text-left text-[#f3fffa] pl-2 font-bold text-[25px] mb-2">গ্রাহক সন্তুষ্ট</p><br><br>
+            <p class="text-right text-[#ebff3d] font-bold text-[45px] mb-4 pr-[10px]">৯৫%</p>
+            <hr class="w-full border-[#f3fffa] mb-4">
+            <p class="text-center text-[#f3fffa] text-[17px]">৯৫% গ্রাহক আমাদের সেবা ও পণ্যে সন্তুষ্ট।</p>
+        </div>
+    </div>
+    <br><br>
+    <div class="flex flex-col">
+            <p class="font-bold flex justify-center items-center border-[1px] bg-[#ebff3d] rounded-[45px] w-[400px] mx-auto text-[#1b4b36] text-[28px]">
+                অর্ডার করতে ক্লিক করুন 
+            <img class="h-[35px] w-[45px] ml-2" src="E:\Html CSS\BD-Funnel-Builder-task_tailwind_css\assets\css\green-icon.png">
+            </p>
+    </div>
+
+    
+</section>
+
+<footer class="bg-[#0b3b0b] text-white py-5 text-center font-sans">
+    <div class="flex flex-wrap justify-around items-center max-w-[1200px] mx-auto px-5 py-5">
+        
+        <div class="flex items-center gap-2">
+            <img src="E:\Html CSS\Task1\assets\Vector (3).png" alt="Phone Icon" class="w-6 h-6">
+            <span class="text-[16px] font-semibold">+880 123 456 790</span>
+        </div>
+        
+        
+        <div class="flex items-center">
+            <img src="E:\Html CSS\Task1\assets\logo.png" alt="Fast Service Logo" class="w-[100px]">
+        </div>
+        
+        
+        <div class="flex items-center gap-2">
+            <a href="#"><img src="E:\Html CSS\Task1\assets\Vector.jpg" alt="Facebook" class="bg-white rounded-full p-1 w-8 h-8 hover:scale-110 transition-transform duration-300"></a>
+            <a href="#"><img src="E:\Html CSS\Task1\assets\Vector (1).png" alt="YouTube" class="bg-white rounded-full p-1 w-8 h-8 hover:scale-110 transition-transform duration-300"></a>
+            <a href="#"><img src="E:\Html CSS\Task1\assets\Vector (2).png" alt="TikTok" class="bg-white rounded-full p-1 w-8 h-8 hover:scale-110 transition-transform duration-300"></a>
+        </div>
+    </div>
+
+    <div class="bg-[#092a07] py-2 text-[#f5f5f5] text-sm">
+        <p>© 2024 All Rights Reserved by <strong class="text-[#ffffff]">BDFunnelBuilder</strong>. Unauthorized use prohibited.</p>
+    </div>
+</footer>
+
+
 </template>
